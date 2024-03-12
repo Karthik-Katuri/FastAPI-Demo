56ddb040faa681aa94c447dd808d4214a90a645c
Karthik-Katuri <kkaturi2003@gmail.com>
2024-03-12 23:06:11 +0530

    FastAPI demo

---
+++ Python_fastapi/working.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 26 19:49:27 2024
+
+@author: akash
+"""
+from typing import Optional
+from fastapi import FastAPI, Path, Query, HTTPException, status
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    brand : Optional[str]= None
+    
+class UpdateItem(BaseModel):
+    name: Optional[str]= None
+    price: Optional[float]= None
+    brand : Optional[str]= None    
+    
+inventory = {
+    }
+##
+
+@app.get("/get-items-no/")
+def get_items():
+    return inventory
+
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int = Path(..., description="The ID of the item", gt=0)):
+    return inventory[item_id]
+
+
+@app.get("/get-by-name/{item_id}")
+def get_item_by_name(*, item_id = int,name: Optional[str] = None, test = int ):
+    for item_id in inventory:
+        if inventory[item_id].name == name:
+            return inventory[item_id]
+    raise HTTPException(status_code= 404, detail= "Item name not Found")
+
+@app.post("/create-item/{item_id}")
+def create_item(item_id:int, item : Item):
+    if item_id in inventory:
+        raise HTTPException(status_code= 400, detail= "Item ID already Exists")
+
+    inventory[item_id]= item
+    return inventory[item_id]
+
+@app.put("/update-id/{item_id}")
+def update_item(item_id:int, item : UpdateItem):
+    if item_id not in inventory:
+        raise HTTPException(status_code= 404, detail= "Item ID not Found")
+
+    if item.name != None:
+        inventory[item_id].name= item.name
+
+    if item.price != None:
+        inventory[item_id].price= item.price
+
+    if item.brand != None:
+        inventory[item_id].brand= item.brand
+
+
+    return inventory[item_id]
+
+@app.delete("/delete-item")
+def delete_item(item_id: int = Query(... ,description ="the ID of the deleted item is")):
+    if item_id not in inventory:
+        raise HTTPException(status_code= 404, detail= "Item ID not Found")
+
+    del inventory[item_id]
+    return{"Success":"Item deleted!"}
+
+
+        
